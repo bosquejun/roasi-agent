b9eb95d9595aaf73e07ebc762cd2a5700bbf9d34
Jun Bosque <bosque.junpaul@gmail.com>
2026-05-10 18:18:48 +0800

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono, JetBrains_Mono, Inter } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const interHeading = Inter({subsets:['latin'],variable:'--font-heading'});
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontSans.variable, "font-mono", jetbrainsMono.variable, interHeading.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
